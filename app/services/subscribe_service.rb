d59f64337a0c9523e75a5be3f27e783901a6b974
reki <reki.development@gmail.com>
2017-05-30 22:12:34 +0900

    fix merge bug

--- app/services/subscribe_service.rb
+++ app/services/subscribe_service.rb
@@ -8,19 +8,6 @@ class SubscribeService < BaseService
     response     = subscription.subscribe
 
     if response_failed_permanently?(response)
-<<<<<<< HEAD
-      # An error in the 4xx range (except for 429, which is rate limiting)
-      # means we're not allowed to subscribe. Fail and move on
-      account.secret = ''
-      account.save!
-    elsif response_successful?(response)
-      # Anything in the 2xx range means the subscription will be confirmed
-      # asynchronously, we've done what we needed to do
-      account.save!
-    else
-      # What's left is the 5xx range and 429, which means we need to retry
-      # at a later time. Fail loudly!
-=======
       # We're not allowed to subscribe. Fail and move on.
       account.secret = ''
       account.save!
@@ -30,21 +17,12 @@ class SubscribeService < BaseService
     else
       # The response was either a 429 rate limit, or a 5xx error.
       # We need to retry at a later time. Fail loudly!
->>>>>>> tmp-1.4.1
       raise "Subscription attempt failed for #{account.acct} (#{account.hub_url}): HTTP #{response.code}"
     end
   end
 
   private
 
-<<<<<<< HEAD
-  def response_failed_permanently?(response)
-    response.code > 299 && response.code < 500 && response.code != 429
-  end
-
-  def response_successful?(response)
-    response.code > 199 && response.code < 300
-=======
   # Any response in the 3xx or 4xx range, except for 429 (rate limit)
   def response_failed_permanently?(response)
     (response.status.redirect? || response.status.client_error?) && !response.status.too_many_requests?
@@ -53,6 +31,5 @@ class SubscribeService < BaseService
   # Any response in the 2xx range
   def response_successful?(response)
     response.status.success?
->>>>>>> tmp-1.4.1
   end
 end
